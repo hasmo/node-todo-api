d2a61ca5d261bbe27802e2ce453173f1bae4c2ad
Hassan <hassan.mk@hotmail.co.uk>
2019-02-03 12:40:05 -0500

    upd - Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,17 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = 'abc123!';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    // console.log(hash);
+  });
+});
 
-let decoded = jwt.verify(token, '123abxc');
-console.log(decoded);
+var hashedPassword = '$2a$10$ww2ic.dFE8YEOVJaI8hHt.Yc.tKQKhlcOQ1aZRjJFPht4hBFXAIfG';
+
+bcrypt.compare('password', hashedPassword, (err, res) => {
+  console.log(res);
+});
