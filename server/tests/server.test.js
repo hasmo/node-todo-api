fddcff2202c25e96dd8f333a0f46a07bf9987908
Hassan <hassan.mk@hotmail.co.uk>
2019-02-02 22:06:57 -0500

    upd - More tests.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,4 +98,43 @@ describe('GET /todos/:id', () => {
       .expect(404)
       .end(done);
   });
+
+  describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var todoId = todos[0]._id.toHexString();
+
+        request(app)
+          .delete(`/todos/${todoId}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.todo._id).toBe(todoId);
+          })
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+
+            Todo.findById(todoId).then((todo) => {
+              expect(todo).not.toBeTruthy();
+              done();
+            }).catch((e) => done(e));
+          });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+      let todoId = new ObjectID().toHexString();
+
+      request(app)
+        .delete(`/todos/${todoId}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+      request(app)
+        .delete('/todos/111')
+        .expect(404)
+        .end(done);
+    });
+  });
 });
