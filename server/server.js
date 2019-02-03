4bd30dfe862c683573f2bc1eb0b0e70048c77946
Hassan <hassan.mk@hotmail.co.uk>
2019-02-02 22:16:31 -0500

    upd - Add PATCH endpoint.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let {ObjectId} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
+const _ = require('lodash');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -62,6 +63,34 @@ app.delete('/todos/:id', (req, res) => {
     }
 
     return res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    // JS timestamp.
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
